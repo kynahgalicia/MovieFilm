40a3ccde7022b28df8b63cd2e00e97ab454ce38e
kynahgalicia <shekinahgalicia@gmail.com>
2021-01-25 23:35:56 +0800

    migrations fixed

--- database/migrations/2021_01_13_120820_create_relationship_table.php
+++ database/migrations/2021_01_13_120820_create_relationship_table.php
@@ -18,7 +18,6 @@ class CreateRelationshipTable extends Migration
             $table->string('name');
             $table->date('birthday');
             $table->longText('notes');
-            // $table->string('images');
             $table->timestamps();
         });
         Schema::create('producers', function (Blueprint $table) {
@@ -47,16 +46,7 @@ class CreateRelationshipTable extends Migration
             $table->foreign('genre_id')->references('genre_id')->on('genres')->onDelete('cascade')->onUpdate('cascade');
             $table->bigInteger('producer_id')->unsigned();
             $table->foreign('producer_id')->references('producer_id')->on('producers')->onDelete('cascade')->onUpdate('cascade');
-            // $table->string('images');
             $table->timestamps();
-        });
-        Schema::create('actor_movie_roles', function (Blueprint $table) {
-            $table->bigInteger('actor_id')->unsigned();
-            $table->foreign('actor_id')->references('actor_id')->on('actors')->onDelete('cascade')->onUpdate('cascade');
-            $table->bigInteger('movie_id')->unsigned();
-            $table->foreign('movie_id')->references('movie_id')->on('movies')->onDelete('cascade')->onUpdate('cascade');
-            $table->bigInteger('role_id')->unsigned();
-            $table->foreign('role_id')->references('role_id')->on('roles')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
@@ -67,7 +57,6 @@ class CreateRelationshipTable extends Migration
      */
     public function down()
     {
-        Schema::drop('actor_movie_roles');
         Schema::drop('movies');
         Schema::drop('roles');
         Schema::drop('genres');
